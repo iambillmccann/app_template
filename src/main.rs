2c3c12de3b51841b815d6071a3287e1288abffe3
Bill McCann <bill.mccann@gmail.com>
2024-05-01 13:27:36 -0400

    Added a little ... very little ... styling.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use crate::components::Preview;
 use crate::components::Stories;
 use crate::types::PreviewState;
 
+// Urls are relative to your Cargo.toml file
+const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
+
 fn main() {
     launch(App);
 }
@@ -18,9 +21,9 @@ pub fn App() -> Element {
     use_context_provider(|| Signal::new(PreviewState::Unset));
 
     rsx! {
-        div { display: "flex", flex_direction: "row", width: "100%",
-            div { width: "50%", Stories {} }
-            div { width: "50%", Preview {} }
-        }
+        div { class: "flex w-full",
+        div { class: "w-1/2 border border-gray-300 bg-gray-50 p-4 m-4", Stories {} }
+        div { class: "w-1/2 border border-gray-300 bg-gray-50 p-4 m-4", Preview {} }
+    }
     }
 }
