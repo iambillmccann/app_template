a18681ae113021d202771223a598b72a7c5f2083
Bill McCann <bill.mccann@gmail.com>
2024-05-12 21:03:58 -0400

    Reading .env but not the variable

--- src/pages/app_settings.rs
+++ src/pages/app_settings.rs
@@ -1,43 +1,39 @@
 // src/pages/landing.rs
+extern crate dotenv;
 
-use config::{Config, Environment, File};
-use dioxus::prelude::*;
-use std::collections::HashMap;
+use dioxus::{html::p, prelude::*};
+use dotenv::dotenv;
+use std::env;
+// use config::{Config, Environment};
 
 #[component]
 pub fn AppSettingsPage(items: String) -> Element {
-    let mut settings = Config::default();
+    // let mut settings = Config::default();
     let mut message: String = String::from("This is Ok."); // Change the type of `message` to `String`
-    let config_path = format!("{}/config", env!("CARGO_MANIFEST_DIR"));
 
-    match settings.merge(File::with_name(&config_path)) {
-        Ok(_) => (),
-        Err(e) => message = format!("Failed to load the config file: {}", e),
-    }
-
-    match settings.merge(Environment::with_prefix("APP")) {
-        Ok(_) => (),
-        Err(_) => eprintln!("Failed to load environment variables"),
-    }
-
-    let firebase_settings = match settings.get::<HashMap<String, String>>("firebase") {
-        Ok(settings) => settings,
-        Err(e) => {
-            eprintln!("Failed to load Firebase settings: {}", e);
-            HashMap::new() // Use an empty HashMap as the default settings
+    dotenv().ok();
+    match env::var("firebase_project_id") {
+        Ok(firebase_project_id) => {
+            message = format!("Read .env successfully {}", firebase_project_id)
         }
-    };
+        Err(e) => message = format!("Read .env failed, {}", e),
+    }
 
-    let firebase_settings_str = format!("{:?}", firebase_settings);
+    println!("{}", message);
+    // match settings.merge(Environment::with_prefix("APP")) {
+    //     Ok(_) => (),
+    //     Err(_) => eprintln!("Failed to load environment variables"),
+    // }
+
+    // let items_clone = items.clone();
+    // let message_clone = message.clone();
 
     rsx! {
         div {
             class: "flex items-center justify-center h-screen bg-black text-white text-4xl",
             div {
                 p { "Show this: {items}" },
-                p { "Firebase settings: {firebase_settings_str}" }
-                p { "{message}" }
-                p { "{config_path}" }
+                p { "{message}" },
             }
         }
     }
