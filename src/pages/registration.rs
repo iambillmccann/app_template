e5ea0900e710677e34d096debd5cfa7f2e2e11f4
Bill McCann <bill.mccann@gmail.com>
2024-05-17 10:44:58 -0400

    Playing with Tailwind styling (unsuccessfully)

--- src/pages/registration.rs
+++ src/pages/registration.rs
@@ -4,11 +4,14 @@ use dioxus::prelude::*;
 pub fn RegistrationPage() -> Element {
     rsx! {
         div {
-            class: "flex flex-col items-center justify-center h-screen bg-black text-white",
-            div {
-                h2 { class: "text-xl font-semibold md:text-2xl", "Sign up" }
-                p { class: "text-secondary", "Already have an account? Sign in." }
-                RegistrationForm {}
+            class: "flex flex-col md:flex-row-reverse md:h-screen items-center justify-center h-screen bg-black text-white",
+            section {
+                class: "justify-center px-4 md:px-0 md:flex md:w-2/3 md:border-r",
+                div {
+                    h2 { class: "text-xl font-semibold md:text-2xl", "Sign up" }
+                    p { class: "text-secondary", "Already have an account? Sign in." }
+                    RegistrationForm {}
+                }
             }
         }
     }
